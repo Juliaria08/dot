6dd4babae13723eb19f3ea7c8869add66419d21f
Julian Jarabo <julianjar08@gmail.com>
2020-10-18 13:03:01 +0200

    Xmenu.sh update

--- .local/bin/xmenu.sh
+++ .local/bin/xmenu.sh
@@ -3,33 +3,34 @@
 cat <<EOF | xmenu | sh &
 Terminal	st
 Web Browser	brave
+Calendar	st -e 'calcurse'
 
-Cli Apps
-	Mail
-		Mail	st -e 'neomutt'
-		Sync Mail	mw -Y
+Productivity
+	Mail	st -e 'neomutt'
+	Sync Mail	mw -Y
 	Calendar	st -e 'calcurse'
-	PulseMixer	st -e 'pulsemixer'
-	Music Player	st -e 'ncmpcpp'
-	File Managers
-		lf	st -e 'lf'
-		vifm	st -e 'vifm'
+Entretaiment
+	Youtube	st -e 'youtube-viewer'
+	Image editor	gimp
+	Char Map	gucharmap
 	Recording
 		Camera	mpv --no-cache --no-osc --no-input-default-bindings --input-conf=/dev/null --title=webcam $(ls /dev/video[0,2,4,6,8] | tail -n 1)
 		Record	dmenurecord
 		Stop Recording	dmenurecord kill
 		Toogle screenkey	killall screenkey || screenkey &
-	Youtube	st -e 'youtube-viewer'
-	Config
-		Network Manager TUI	sudo -A st -e nmtui
-Gui Apps
-	* Managers
-		Password Managers
-			Pass	passmenu
-			Keepass	keepassxc
-		File Manager	pcmanfm
-		Wallpaper Manager	nitrogen
-	Image editor	gimp
-	Char Map	gucharmap
-
+	Music
+		Ncmpcpp	st -e 'ncmpcpp'
+Config / SysMonitors
+	PulseMixer	st -e 'pulsemixer'
+	Htop	st -e 'htop'
+	Network Manager TUI	sudo -A st -e nmtui
+* Managers
+	Password Managers
+		Pass	passmenu
+		Keepass	keepassxc
+	File Managers
+		lf	st -e 'lf'
+		vifm	st -e 'vifm'
+		pcmanfm	pcmanfm
+	Wallpaper Manager	nitrogen
 EOF
