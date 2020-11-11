90cd81b39347a9724f92e7881c9996cf5b0e79ea
Julian Jarabo <julianjar08@gmail.com>
2020-11-11 15:01:10 +0100

    Update emacs conf.

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -72,6 +72,28 @@
 
 ;; A good introduction https://tecosaur.github.io/emacs-config/config.html.
 
-(setq-default custom-file (expand-file-name ".custom.el" doom-private-dir))
-(when (file-exists-p custom-file)
-  (load custom-file))
+;; CalcTex
+(package! calctex :recipe (:host github :repo "johnbcoughlin/calctex"
+                           :files ("*.el" "calctex/*.el" "calctex-contrib/*.el" "org-calctex/*.el"))
+  :pin "7fa2673c64...")
+
+;; Latex
+(package! auto-activating-snippets :recipe
+  (:host github :repo "ymarco/auto-activating-snippets")
+  :pin "8a3ef55002...")
+(package! latex-auto-activating-snippets
+  :recipe (:local-repo "lisp/LaTeX-auto-activating-snippets"))
+
+;; Org
+;; Mode
+(unpin! org-mode)
+;; Agenda
+(package! org-super-agenda :pin "3264255989...")
+;; Doct
+(package! doct
+  :recipe (:host github :repo "progfolio/doct")
+  :pin "dabb30ebea...")
+(package! org-pretty-table-mode
+  :recipe (:host github :repo "Fuco1/org-pretty-table") :pin "88380f865a...")
+(package! org-fragtog :pin "92119e3ae7...")
+(package! org-pretty-tags :pin "40fd72f3e7...")
