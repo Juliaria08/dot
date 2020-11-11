77bd51825781af2ab9809c1b673cdef1665557fd
Julian Jarabo <julianjar08@gmail.com>
2020-11-11 18:39:22 +0100

    Update init.el ( Emacs )

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -177,7 +177,6 @@
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        web               ; the tubes
-       exwm
        yaml              ; JSON, but readable
 
        :email
