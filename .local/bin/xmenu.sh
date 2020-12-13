dac9c0e81e7a7aa0adb6cc9a98083b2acc6cec20
Julian Jarabo <julianjar08@gmail.com>
2020-12-13 08:43:45 +0100

    Add mumble to xmenu

--- .local/bin/xmenu.sh
+++ .local/bin/xmenu.sh
@@ -16,6 +16,7 @@ Entretaiment
 	Char Map	gucharmap
 	 Chromium	~/Documents/apps/ugc.AppImage
 	Libre Sprite	~/Documents/apps/LibreSprite.AppImage
+	Mumble	mumble
 	Recording
 		Camera	mpv --no-cache --no-osc --no-input-default-bindings --input-conf=/dev/null --title=webcam $(ls /dev/video[0,2,4,6,8] | tail -n 1)
 		Record	dmenurecord
